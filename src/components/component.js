a4086f8666572a55bdd0e10e24f4caacd63cb90b
Renzo Damian <renzo.damian.go@gmail.com>
2022-09-08 23:49:41 -0500

    Install webpack

---
+++ src/components/component.js
@@ -0,0 +1,7 @@
+export const saludar = (nombre) => {
+	console.log(nombre);
+	const h1 = document.createElement('h1');
+	h1.innerText = `Hola ${nombre}`;
+	document.body.append(h1);
+	console.log("create element");
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-const saludar = (nombre) => {
-	console.log(nombre);
-	const h1 = document.createElement('h1');
-	h1.innerText = `Hola ${nombre}`;
-	document.body.append(h1);
-	console.log("create element");
-};
+import { saludar } from "./components/component";
 
 const nombre = "Fernando";
 
