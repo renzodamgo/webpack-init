ba59109a144403720f29398cc603bc334b1cfd7e
Renzo Damian <renzo.damian.go@gmail.com>
2022-09-11 01:34:57 -0500

    add webpack config Html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,28 @@
+const HtmlWebpack = require('html-webpack-plugin');
+
 module.exports = {
 	mode: 'production',
+	output: {
+		clean: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				loader: 'html-loader',
+				options: {
+					sources: false,
+					
+
+				}
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpack({
+			//title: 'Mi Webpack App',
+			//filename: 'index.html',
+			template: 'src/index.html'
+		})
+	]
 };
